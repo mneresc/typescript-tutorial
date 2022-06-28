f977857c11395c69c8904cd6f04d5ce947e04ec4
Neres <mneresc@gmail.com>
2022-06-28 17:41:10 -0300

    alterado pra readonly os métodos para que possa remover

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,21 +1,9 @@
 
 export class Negociacao {
 
-    constructor(private _data: Date,private _quantidade: number,private _valor: number) {}
-
-    get data() {
-        return this._data;
-    }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
+    constructor(public readonly data: Date,public readonly quantidade: number,public readonly valor: number) {}
 
     get volume() {
-        return this._valor * this._quantidade;
+        return this.valor * this.quantidade;
     }
 }
