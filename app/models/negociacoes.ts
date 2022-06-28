b93bba6918fd07c825e5fac36e3dac2ed8bb9338
Neres <mneresc@gmail.com>
2022-06-28 17:37:56 -0300

    Mudando para shgar sintaxe de array

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes:Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
-    add(negociacao:Negociacao){
+    add(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    list():ReadonlyArray<Negociacao>{
-       return this.negociacoes;
+    list(): ReadonlyArray<Negociacao> {
+        return this.negociacoes;
     }
 }
