ae7b2dd5a0efc599b1021ac57ad1c6669d22ea1f
Neres <mneresc@gmail.com>
2022-06-28 17:50:46 -0300

    Alterando tipo data para evitar alteralcao do tipo

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,9 +1,13 @@
 
 export class Negociacao {
 
-    constructor(public readonly data: Date,public readonly quantidade: number,public readonly valor: number) {}
+    constructor(private _date: Date, public readonly quantidade: number, public readonly valor: number) { }
 
     get volume() {
         return this.valor * this.quantidade;
+    }
+
+    get data(): Date {
+        return new Date(this._date.getTime());
     }
 }
