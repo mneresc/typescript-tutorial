36513f686aa8b997299a0afca6d9436a7e795a46
Neres <mneresc@gmail.com>
2022-07-01 15:31:47 -0300

    Implementado decorator

--- app/decorators/log-tempo-execucao.ts
+++ app/decorators/log-tempo-execucao.ts
@@ -1,5 +1,13 @@
 export function logarTempoExeucao() {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor){
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function (...args: any[]) {
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`Tempo de excução ${propertyKey}: ${(t2 - t1)} ms`);
+            return retorno;
+        };
         return descriptor;
     }
 }
