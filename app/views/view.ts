1d5a6aa89dbe5f39ef5ebd5e34edf6e60b79cf95
Neres <mneresc@gmail.com>
2022-07-01 14:45:07 -0300

    Adicionando parametro de scape

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,17 +1,24 @@
 
 export abstract class View<T> {
     protected elemento: HTMLElement;
+    protected escape: boolean = false;
 
-    constructor(selector: string) {
+    constructor(selector: string, escape?: boolean) {
         this.elemento = document.querySelector(selector);
-
+        if (escape) {
+            this.escape = escape;
+        }
     }
 
-        
-    update( model: T): void {
+
+    update(model: T): void {
+        let template = this.template(model);
+        if (this.escape) {
+            template = template.replace('/<script>[\s\S]*?<\/script>/', "")
+        }
         this.elemento.innerHTML = this.template(model);
     }
 
-    protected abstract template(model: T): string ;
+    protected abstract template(model: T): string;
 
 }
